1b020c84c46202512dddfdda73e3c10e57f3751e
phatsngoo <phatsngoo2702@gmail.com>
2018-03-18 22:47:20 +0700

    First Commit

---
+++ Assets/Scripts/Tutorial.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+using UnityEngine.SceneManagement;
+public class Tutorial : MonoBehaviour
+{
+    private void Awake()
+    {
+        Advertisement.Show();
+    }
+    public AudioClip ButtonClick;
+    public void Home()
+    {
+        AudioSource.PlayClipAtPoint(ButtonClick, Vector3.zero);
+        SceneManager.LoadScene("StartScene");
+    }
+}
